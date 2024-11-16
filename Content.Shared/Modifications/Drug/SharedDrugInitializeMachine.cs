2343908752a91b9105aa617e578b03577f4274a4
KloopRE <100780612+KloopRE@users.noreply.github.com>
2024-11-16 16:08:53 +0300

    Наркотики
    Машина инициализации наркотической зависимости.
    Определяет информацию зависимости сущности через её слюну.

---
+++ Content.Shared/Modifications/Drug/SharedDrugInitializeMachine.cs
@@ -0,0 +1,17 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Modifications.Drug;
+
+[Serializable, NetSerializable]
+public enum DrugInitializeMachineVisualState
+{
+    Idle,
+    Running
+}
+
+public enum DrugInitializeMachineVisualizerLayers : byte
+{
+    Base,
+    BaseUnlit
+}
+
